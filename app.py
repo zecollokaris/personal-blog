3de59e699a5552940bde0ef350773b7715553477
zecollokaris <collo.kariss@gmail.com>
2018-04-21 16:16:50 +0300

    added route for about

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#Route for about
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
