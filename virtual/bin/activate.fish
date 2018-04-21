08fef5adfb9d0e849507e16f166cc7c286a77698
zecollokaris <collo.kariss@gmail.com>
2018-04-21 21:23:03 +0300

    added static directory

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/karis/Desktop/personal-blog/virtual"
+set -gx VIRTUAL_ENV "/home/karis/Desktop/personal-blog-project/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
