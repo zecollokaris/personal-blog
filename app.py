f809d7ef8a78336cac6028e80edea78f630648ac
zecollokaris <collo.kariss@gmail.com>
2018-04-21 16:15:33 +0300

    imported flask

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-from flask import Flask
+#Importing flask and render_template
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
