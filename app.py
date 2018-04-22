5b4f4ecaeb52442d62c631f4b96a9b981069752a
zecollokaris <collo.kariss@gmail.com>
2018-04-22 19:11:30 +0300

    added sqalchemy dependancies due to failure in running app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 #Importing Flask-SQLAlchemy for database setup.
-from flask_sqlalchemy import SQAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -10,6 +10,19 @@ app = Flask(__name__)
 app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:////Home/Desktop/personal-blog-project/blog.db'
 
 db = SQAlchemy(app)
+
+#Creating table that holds information
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+
+    
+
+
 
 #Route for index
 @app.route('/')
